a00e735410e235676f138f005fed8d879468716a
Attila Bartha <atikenny@gmail.com>
2025-06-11 14:08:29 +0200

    add filter count

--- packages/coffee.chealt.com/src/components/home/coffee-filters.js
+++ packages/coffee.chealt.com/src/components/home/coffee-filters.js
@@ -1,11 +1,23 @@
 class CoffeeFilters extends HTMLElement {
   connectedCallback() {
     this.toggleButton = this.querySelector('.toggle-button');
+    this.toggleText = this.querySelector('.toggle-text');
     this.clearButton = this.querySelector('.clear-button');
     this.filters = this.querySelector('.filters');
+    this.supElement = this.querySelector('sup');
 
+    this.addFilterChangeListeners();
     this.clearFilters();
     this.toggleFilters();
+    this.showNumberOfActiveFilters();
+  }
+
+  addFilterChangeListeners() {
+    this.querySelectorAll('[type=checkbox]').forEach((input) => {
+      input.addEventListener('change', () => {
+        this.showNumberOfActiveFilters();
+      });
+    });
   }
 
   clearFilters() {
@@ -13,6 +25,7 @@ class CoffeeFilters extends HTMLElement {
       this.querySelectorAll('[type=checkbox]').forEach((input) => {
         input.checked = false;
       });
+      this.showNumberOfActiveFilters();
     });
   }
 
@@ -20,6 +33,18 @@ class CoffeeFilters extends HTMLElement {
     this.toggleButton.addEventListener('click', () => {
       this.filters.classList.toggle('active');
     });
+  }
+
+  showNumberOfActiveFilters() {
+    const numberOfActiveFilters = this.querySelectorAll('[type=checkbox]:checked').length;
+
+    if (numberOfActiveFilters === 0) {
+      this.supElement.classList.add('hidden');
+    } else {
+      this.supElement.classList.remove('hidden');
+    }
+
+    this.supElement.textContent = numberOfActiveFilters;
   }
 }
 
