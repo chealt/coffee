b4668c8fca0fa89a012a6826a75cc55c602d55b5
Attila Bartha <atikenny@gmail.com>
2025-06-13 13:28:17 +0200

    include OCR in separate astro file

--- packages/coffee.chealt.com/src/middleware.js
+++ packages/coffee.chealt.com/src/middleware.js
@@ -2,12 +2,9 @@ const onRequest = async (context, next) => {
   const userAgent = context.request.headers.get('user-agent');
   const isIOS = /iPad|iPhone|iPod/u.test(userAgent);
 
-  console.log(`userAgent: ${userAgent}`);
-  console.log(`isIOS: ${isIOS}`);
-
   context.locals.clientSideOCR = !isIOS;
 
-  console.log(`context.locals.clientSideOCR: ${context.locals}`);
+  console.log(`context.locals.clientSideOCR: ${context.locals.clientSideOCR}`); // eslint-disable-line no-console
 
   return next();
 };
