b10a9614f69e2bbce424bc5ea58f76dd16c3efda
Attila Bartha <atikenny@gmail.com>
2025-06-13 13:19:49 +0200

    log user agent and context locals

--- packages/coffee.chealt.com/src/middleware.js
+++ packages/coffee.chealt.com/src/middleware.js
@@ -2,11 +2,12 @@ const onRequest = async (context, next) => {
   const userAgent = context.request.headers.get('user-agent');
   const isIOS = /iPad|iPhone|iPod/u.test(userAgent);
 
-  console.log(userAgent);
+  console.log(`userAgent: ${userAgent}`);
+  console.log(`isIOS: ${isIOS}`);
 
   context.locals.clientSideOCR = !isIOS;
 
-  console.log(context.locals.clientSideOCR);
+  console.log(`context.locals.clientSideOCR: ${context.locals}`);
 
   return next();
 };
