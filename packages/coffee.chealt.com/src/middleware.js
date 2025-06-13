0f0c3662889f8aa3e4ab28e31c0b64e9e5f209be
Attila Bartha <atikenny@gmail.com>
2025-06-13 12:55:41 +0200

    enable logging, log user agent and clientSideOCR

--- packages/coffee.chealt.com/src/middleware.js
+++ packages/coffee.chealt.com/src/middleware.js
@@ -2,6 +2,9 @@ const onRequest = async (context, next) => {
   const userAgent = context.request.headers.get('user-agent');
   const isIOS = /iPad|iPhone|iPod/u.test(userAgent);
 
+  console.log(userAgent);
+  console.log(isIOS);
+
   context.locals.clientSideOCR = !isIOS;
 
   return next();
