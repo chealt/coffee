540904a9ed6f7ae30cb4fe2924ad6801a37210b3
Attila Bartha <atikenny@gmail.com>
2025-06-28 15:19:31 +0200

    handle auth errors

--- packages/coffee.chealt.com/src/pages/api/authentication/login/options.json.js
+++ packages/coffee.chealt.com/src/pages/api/authentication/login/options.json.js
@@ -3,12 +3,18 @@ import { getAuthenticationOptions } from '../../../../server/database/user.js';
 const POST = async ({ request }) => {
   const { username } = await request.json();
 
-  const options = await getAuthenticationOptions(username);
+  try {
+    const options = await getAuthenticationOptions(username);
 
-  return new Response(JSON.stringify({ options }), {
-    status: 200,
-    headers: { 'Content-Type': 'application/json' }
-  });
+    return new Response(JSON.stringify({ options }), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' }
+    });
+  } catch (error) {
+    console.error(error); // eslint-disable-line no-console
+
+    return new Response(JSON.stringify({ error: 'Cannot get authentication options. Please try again!' }));
+  }
 };
 
 export { POST };
--- packages/coffee.chealt.com/src/pages/api/authentication/registration/options.json.js
+++ packages/coffee.chealt.com/src/pages/api/authentication/registration/options.json.js
@@ -3,12 +3,18 @@ import { getRegistrationOptions } from '../../../../server/database/user.js';
 const POST = async ({ request }) => {
   const { username } = await request.json();
 
-  const options = await getRegistrationOptions(username);
+  try {
+    const options = await getRegistrationOptions(username);
 
-  return new Response(JSON.stringify({ options }), {
-    status: 200,
-    headers: { 'Content-Type': 'application/json' }
-  });
+    return new Response(JSON.stringify({ options }), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' }
+    });
+  } catch (error) {
+    console.error(error); // eslint-disable-line no-console
+
+    return new Response(JSON.stringify({ error: 'Cannot get registration options. Please try again!' }));
+  }
 };
 
 export { POST };
