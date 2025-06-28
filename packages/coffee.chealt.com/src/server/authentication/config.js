43a94f7a782138fa6265e8f48fc6bef15e342cf7
Attila Bartha <atikenny@gmail.com>
2025-06-28 16:15:23 +0200

    Update config.js

--- packages/coffee.chealt.com/src/server/authentication/config.js
+++ packages/coffee.chealt.com/src/server/authentication/config.js
@@ -1,6 +1,6 @@
 const relyingPartyName = 'Chealt Coffee';
-const relyingPartyID = import.meta.env?.RELYING_PARTY_ID;
-const origin = import.meta.env?.ORIGIN;
+const relyingPartyID = import.meta.env?.RELYING_PARTY_ID || process.env.RELYING_PARTY_ID;
+const origin = import.meta.env?.ORIGIN || process.env.ORIGIN;
 const sessionSecret = import.meta.env?.SESSION_SECRET || process.env.SESSION_SECRET;
 const cookieNameUsername = 'coffee-chealt-username';
 const cookieNameSession = 'coffee-chealt-session';
