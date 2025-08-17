f545fae39c51be7bc5f30d91b212e1cf3b9bc0ee
Attila Bartha <atikenny@gmail.com>
2025-08-17 07:31:29 +0200

    remove unnecessary workbox configs

--- packages/coffee.chealt.com/astro.config.js
+++ packages/coffee.chealt.com/astro.config.js
@@ -22,11 +22,7 @@ export default defineConfig({
     // eslint-disable-next-line new-cap
     AstroPWA({
       registerType: 'autoUpdate',
-      injectRegister: 'auto',
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,webp}'],
-        globIgnores: ['_worker.js/**/*'],
-        navigateFallback: '/',
         runtimeCaching: [
           {
             urlPattern: ({ request }) => request.mode === 'navigate',
