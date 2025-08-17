db903bd510e9d585d76186ff36c17fd6f8b9df60
Attila Bartha <atikenny@gmail.com>
2025-08-17 12:04:35 +0200

    use manual service worker

--- packages/coffee.chealt.com/astro.config.js
+++ packages/coffee.chealt.com/astro.config.js
@@ -22,28 +22,17 @@ export default defineConfig({
     // eslint-disable-next-line new-cap
     AstroPWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'favicon.ico', 'offline.html'],
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
-        globIgnores: ['_worker.js/**/*'],
-        navigateFallback: '/offline.html',
-        runtimeCaching: [
-          {
-            urlPattern: /^\/[^.]*$/u,
-            handler: 'NetworkFirst',
-            options: {
-              cacheName: 'pages-cache',
-              networkTimeoutSeconds: 10,
-              cacheableResponse: {
-                statuses: [200]
-              }
-            }
-          }
-        ]
-      },
       manifest: false,
       devOptions: {
         enabled: true
+      },
+      includeAssets: ['favicon.svg', 'favicon.ico', 'offline.html'],
+      strategies: 'injectManifest',
+      srcDir: 'src',
+      filename: 'sw.js',
+      workbox: {
+        globIgnores: ['_worker.js/**/*'],
+        navigateFallback: '/offline.html'
       }
     })
   ],
---
+++ packages/coffee.chealt.com/src/sw.js
@@ -0,0 +1,31 @@
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+import { precacheAndRoute } from 'workbox-precaching';
+import { registerRoute } from 'workbox-routing';
+import { NetworkFirst, StaleWhileRevalidate } from 'workbox-strategies';
+
+// This placeholder will be replaced by the PWA plugin with a list of
+// your static assets (JS, CSS, etc.) to precache.
+precacheAndRoute(self.__WB_MANIFEST); // eslint-disable-line no-underscore-dangle
+
+const pageStrategy = new NetworkFirst({
+  cacheName: 'pages-cache',
+  networkTimeoutSeconds: 5,
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [200]
+    })
+  ]
+});
+
+registerRoute(({ request }) => request.mode === 'navigate', pageStrategy);
+
+const assetStrategy = new StaleWhileRevalidate({
+  cacheName: 'assets-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [200]
+    })
+  ]
+});
+
+registerRoute(({ request }) => request.destination === 'image', assetStrategy);
