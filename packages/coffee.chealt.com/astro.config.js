21ba731d4b9dda398bf82192796fd390b5d99d3d
Attila Bartha <atikenny@gmail.com>
2025-08-29 13:09:07 +0200

    only use process env for env vars

--- packages/coffee.chealt.com/astro.config.js
+++ packages/coffee.chealt.com/astro.config.js
@@ -27,9 +27,6 @@ export default defineConfig({
     build: {
       sourcemap: true,
       assetsInlineLimit: 0
-    },
-    define: {
-      'process.env.TURSO_DATABASE_URL_ATTILABARTHA': process.env.TURSO_DATABASE_URL_ATTILABARTHA
     }
   },
   adapter: cloudflare({
