45a1872737ecd912ffd0faf25568df33484522d4
Attila Bartha <atikenny@gmail.com>
2025-09-22 15:32:50 +0200

    update env vars in wrangler, add new registration code sending cript

--- packages/coffee-admin/src/users/email-content.js
+++ packages/coffee-admin/src/users/email-content.js
@@ -1,6 +1,6 @@
 const content = ({ username, registrationCode }) => `
   <h1>Hello!</h1>
-  <p>Register on coffee.chealt.com by following <a href="https://coffee.chealt.com/registration/${username}?code=${registrationCode}">this link</a>.</p>
+  <p>Register on Central Beans by following <a href="https://centralbeans.com/registration/${username}?code=${registrationCode}">this link</a>.</p>
   <p>This link will work for the next 24 hours. Contact support at <a href="mailto:info@chealt.com">info@chealt.com</a> to receive a new registration link.</p>
 `;
 
---
+++ packages/coffee-admin/src/users/send-registration-code.js
@@ -0,0 +1,64 @@
+/* eslint-disable camelcase, no-console */
+import { createClient } from '@libsql/client';
+import { createClient as createPlatformClient } from '@tursodatabase/api';
+import jwt from 'jsonwebtoken';
+
+import { sendEmail } from './email.js';
+
+if (!process.argv?.some((arg) => arg.includes('--username='))) {
+  throw new Error('Username is not set');
+}
+
+if (!process.argv?.some((arg) => arg.includes('--email='))) {
+  throw new Error('Email is not set');
+}
+
+const username = process.argv.find((arg) => arg.includes('--username')).replace('--username=', '');
+const email = process.argv.find((arg) => arg.includes('--email')).replace('--email=', '');
+
+const token = process.env.TURSO_API_TOKEN;
+const defaultToken = process.env.TURSO_DEFAULT_TOKEN;
+const sessionSecret = process.env.SESSION_SECRET;
+
+if (!token) {
+  throw new Error('TURSO_API_TOKEN is not set');
+}
+
+if (!defaultToken) {
+  throw new Error('TURSO_DEFAULT_TOKEN is not set');
+}
+
+if (!sessionSecret) {
+  throw new Error('SESSION_SECRET is not set');
+}
+
+const platformClient = createPlatformClient({
+  org: 'chealt',
+  token
+});
+
+const { hostname } = await platformClient.databases.get(username);
+
+const url = `libsql://${hostname}`;
+
+console.info('creating user DB token...');
+const { jwt: authToken } = await platformClient.databases.createToken(username, {
+  authorization: 'full-access'
+});
+
+const userClient = createClient({
+  url,
+  authToken
+});
+
+console.info('creating registration code...');
+const registrationCode = jwt.sign({ username }, sessionSecret, { expiresIn: '24h' });
+
+console.info('updating DB registration code...');
+await userClient.execute({
+  sql: 'UPDATE users SET registration_code = :registrationCode WHERE name = :username AND email = :email',
+  args: { username, registrationCode, email }
+});
+
+console.info(`sending email with registration code: ${registrationCode} to email: ${email}`);
+await sendEmail({ to: email, username, registrationCode });
