1e9c24143af8d781c46fa3d59e0d4bc582faa48f
Attila Bartha <atikenny@gmail.com>
2025-09-29 14:35:02 +0200

    update domain in the email content

--- packages/coffee-admin/src/users/email-content.js
+++ packages/coffee-admin/src/users/email-content.js
@@ -1,7 +1,7 @@
 const content = ({ username, registrationCode }) => `
   <h1>Hello!</h1>
   <p>Register on Central Beans by following <a href="https://centralbeans.com/registration/${username}?code=${registrationCode}">this link</a>.</p>
-  <p>This link will work for the next 24 hours. Contact support at <a href="mailto:info@chealt.com">info@chealt.com</a> to receive a new registration link.</p>
+  <p>This link will work for the next 24 hours. Contact support at <a href="mailto:info@centralbeans.com">info@centralbeans.com</a> to receive a new registration link.</p>
 `;
 
 export default content;
