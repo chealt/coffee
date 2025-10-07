c2eb5fa3c63f381ecb4e209a712699c281961a33
Attila Bartha <atikenny@gmail.com>
2025-10-07 15:28:08 +0200

    add taste note links to coffee descriptions

--- packages/coffee.chealt.com/src/components/coffees/utils.js
+++ packages/coffee.chealt.com/src/components/coffees/utils.js
@@ -34,10 +34,7 @@ const getDetails =
     images: coffeeImages.filter(({ coffee_id: id }) => id === coffee.id).map((coffeeImage) => coffeeImage.url),
     tasteNotes: coffeeTasteNotes
       .filter(({ coffee_id: id }) => id === coffee.id)
-      .map(
-        ({ taste_note_id: tasteNoteId }) =>
-          tasteNotes.find(({ taste_note_id: id, language_code: code }) => tasteNoteId === id && code === locale)?.name
-      ),
+      .map(({ taste_note_id: tasteNoteId }) => tasteNotes.find(({ taste_note_id: id }) => id === tasteNoteId)),
     isDecaf: Boolean(coffee.is_decaf),
     originCountry: originCountries.find(
       (originCountry) =>
