7745c1056d256d5c78036f8ae681123bf486d1c8
Attila Bartha <atikenny@gmail.com>
2025-10-21 11:36:19 +0200

    import currencies AWS lambda

--- packages/coffee-admin/index.js
+++ packages/coffee-admin/index.js
@@ -1,6 +1,7 @@
 import { getSecret } from './src/AWS.js';
+import importCurrencies from './src/currencies/import.js';
 
-const supportedFunctions = [];
+const supportedFunctions = ['currencies:import'];
 
 export const handler = async (event) => {
   if (!event.function) {
@@ -13,10 +14,15 @@ export const handler = async (event) => {
 
   const secrets = await getSecret({ name: 'coffeeAdmin' });
 
+  process.env.OPEN_CURRENCY_EXCHANGE_APP_ID = secrets.OPEN_CURRENCY_EXCHANGE_APP_ID;
   process.env.TURSO_DATABASE_URL = secrets.TURSO_DATABASE_URL;
   process.env.TURSO_DEFAULT_TOKEN = secrets.TURSO_DEFAULT_TOKEN;
 
   switch (event.function) {
+    case 'currencies:import':
+      await importCurrencies();
+
+      break;
     default:
       throw new Error(`Unsupported function: ${event.function}, please choose one of ${supportedFunctions.join(', ')}`);
   }
--- packages/coffee-admin/src/currencies/import.js
+++ packages/coffee-admin/src/currencies/import.js
@@ -1,54 +1,58 @@
 /* eslint-disable no-console */
 import { createClient } from '@libsql/client';
 
-const appId = process.env.OPEN_CURRENCY_EXCHANGE_APP_ID;
+const main = async () => {
+  const appId = process.env.OPEN_CURRENCY_EXCHANGE_APP_ID;
 
-if (!appId) {
-  throw new Error('OPEN_CURRENCY_EXCHANGE_APP_ID is not set');
-}
-
-const authToken = process.env.TURSO_DEFAULT_TOKEN;
-const databaseUrl = process.env.TURSO_DATABASE_URL;
-
-if (!databaseUrl) {
-  throw new Error('TURSO_DATABASE_URL is not set');
-}
-
-if (!authToken) {
-  throw new Error('TURSO_DEFAULT_TOKEN is not set');
-}
-
-console.info('Fetching latest rates...');
-const response = await fetch('https://openexchangerates.org/api/latest.json', {
-  headers: {
-    Authorization: `Token ${appId}`
+  if (!appId) {
+    throw new Error('OPEN_CURRENCY_EXCHANGE_APP_ID is not set');
   }
-});
 
-if (!response.ok) {
-  throw new Error(response);
-}
+  const authToken = process.env.TURSO_DEFAULT_TOKEN;
+  const databaseUrl = process.env.TURSO_DATABASE_URL;
 
-const { rates } = await response.json();
+  if (!databaseUrl) {
+    throw new Error('TURSO_DATABASE_URL is not set');
+  }
 
-const client = createClient({
-  url: databaseUrl,
-  authToken
-});
+  if (!authToken) {
+    throw new Error('TURSO_DEFAULT_TOKEN is not set');
+  }
 
-try {
-  console.info('Adding rates to DB...');
-  await client.batch(
-    Object.keys(rates).map((currencyCode) => ({
-      sql: `INSERT INTO exchange_rates (currency_code, rate)
+  console.info('Fetching latest rates...');
+  const response = await fetch('https://openexchangerates.org/api/latest.json', {
+    headers: {
+      Authorization: `Token ${appId}`
+    }
+  });
+
+  if (!response.ok) {
+    throw new Error(response);
+  }
+
+  const { rates } = await response.json();
+
+  const client = createClient({
+    url: databaseUrl,
+    authToken
+  });
+
+  try {
+    console.info('Adding rates to DB...');
+    await client.batch(
+      Object.keys(rates).map((currencyCode) => ({
+        sql: `INSERT INTO exchange_rates (currency_code, rate)
               VALUES (:currencyCode, :rate)
             ON CONFLICT (currency_code) DO UPDATE
               SET rate = :rate`,
-      args: { currencyCode, rate: rates[currencyCode] }
-    }))
-  );
-} catch (error) {
-  throw error;
-}
+        args: { currencyCode, rate: rates[currencyCode] }
+      }))
+    );
+  } catch (error) {
+    throw error;
+  }
 
-console.info('Added rates to the DB');
+  console.info('Added rates to the DB');
+};
+
+export default main;
