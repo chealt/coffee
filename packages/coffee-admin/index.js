99268955633a7ff40cdcd0249f56f19ba953f08c
Attila Bartha <atikenny@gmail.com>
2025-10-21 13:34:48 +0200

    send registration code lambda

--- packages/coffee-admin/index.js
+++ packages/coffee-admin/index.js
@@ -1,7 +1,8 @@
 import { getSecret } from './src/AWS.js';
 import importCurrencies from './src/currencies/import.js';
+import sendRegistrationCode from './src/users/send-registration-code.js';
 
-const supportedFunctions = ['currencies:import'];
+const supportedFunctions = ['currencies:import', 'users:send-registration-code'];
 
 export const handler = async (event) => {
   if (!event.function) {
@@ -15,12 +16,21 @@ export const handler = async (event) => {
   const secrets = await getSecret({ name: 'coffeeAdmin' });
 
   process.env.OPEN_CURRENCY_EXCHANGE_APP_ID = secrets.OPEN_CURRENCY_EXCHANGE_APP_ID;
+  process.env.TURSO_API_TOKEN = secrets.TURSO_API_TOKEN;
   process.env.TURSO_DATABASE_URL = secrets.TURSO_DATABASE_URL;
   process.env.TURSO_DEFAULT_TOKEN = secrets.TURSO_DEFAULT_TOKEN;
+  process.env.SESSION_SECRET = secrets.SESSION_SECRET;
 
   switch (event.function) {
     case 'currencies:import':
       await importCurrencies();
+
+      break;
+    case 'users:send-registration-code':
+      const username = event.username;
+      const email = event.email;
+
+      await sendRegistrationCode({ username, email });
 
       break;
     default:
--- packages/coffee-admin/src/users/send-registration-code.js
+++ packages/coffee-admin/src/users/send-registration-code.js
@@ -5,60 +5,61 @@ import jwt from 'jsonwebtoken';
 
 import { sendEmail } from './email.js';
 
-if (!process.argv?.some((arg) => arg.includes('--username='))) {
-  throw new Error('Username is not set');
-}
+const main = async ({ username, email }) => {
+  if (!username) {
+    throw new Error('Username is not set');
+  }
 
-if (!process.argv?.some((arg) => arg.includes('--email='))) {
-  throw new Error('Email is not set');
-}
+  if (!email) {
+    throw new Error('Email is not set');
+  }
 
-const username = process.argv.find((arg) => arg.includes('--username')).replace('--username=', '');
-const email = process.argv.find((arg) => arg.includes('--email')).replace('--email=', '');
+  const token = process.env.TURSO_API_TOKEN;
+  const defaultToken = process.env.TURSO_DEFAULT_TOKEN;
+  const sessionSecret = process.env.SESSION_SECRET;
 
-const token = process.env.TURSO_API_TOKEN;
-const defaultToken = process.env.TURSO_DEFAULT_TOKEN;
-const sessionSecret = process.env.SESSION_SECRET;
+  if (!token) {
+    throw new Error('TURSO_API_TOKEN is not set');
+  }
 
-if (!token) {
-  throw new Error('TURSO_API_TOKEN is not set');
-}
+  if (!defaultToken) {
+    throw new Error('TURSO_DEFAULT_TOKEN is not set');
+  }
 
-if (!defaultToken) {
-  throw new Error('TURSO_DEFAULT_TOKEN is not set');
-}
+  if (!sessionSecret) {
+    throw new Error('SESSION_SECRET is not set');
+  }
 
-if (!sessionSecret) {
-  throw new Error('SESSION_SECRET is not set');
-}
+  const platformClient = createPlatformClient({
+    org: 'chealt',
+    token
+  });
 
-const platformClient = createPlatformClient({
-  org: 'chealt',
-  token
-});
+  const { hostname } = await platformClient.databases.get(username);
 
-const { hostname } = await platformClient.databases.get(username);
+  const url = `libsql://${hostname}`;
 
-const url = `libsql://${hostname}`;
+  console.info('creating user DB token...');
+  const { jwt: authToken } = await platformClient.databases.createToken(username, {
+    authorization: 'full-access'
+  });
 
-console.info('creating user DB token...');
-const { jwt: authToken } = await platformClient.databases.createToken(username, {
-  authorization: 'full-access'
-});
+  const userClient = createClient({
+    url,
+    authToken
+  });
 
-const userClient = createClient({
-  url,
-  authToken
-});
+  console.info('creating registration code...');
+  const registrationCode = jwt.sign({ username }, sessionSecret, { expiresIn: '24h' });
 
-console.info('creating registration code...');
-const registrationCode = jwt.sign({ username }, sessionSecret, { expiresIn: '24h' });
+  console.info('updating DB registration code...');
+  await userClient.execute({
+    sql: 'UPDATE users SET registration_code = :registrationCode WHERE name = :username AND email = :email',
+    args: { username, registrationCode, email }
+  });
 
-console.info('updating DB registration code...');
-await userClient.execute({
-  sql: 'UPDATE users SET registration_code = :registrationCode WHERE name = :username AND email = :email',
-  args: { username, registrationCode, email }
-});
+  console.info(`sending email with registration code: ${registrationCode} to email: ${email}`);
+  await sendEmail({ to: email, username, registrationCode });
+};
 
-console.info(`sending email with registration code: ${registrationCode} to email: ${email}`);
-await sendEmail({ to: email, username, registrationCode });
+export default main;
