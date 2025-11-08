f14b0cbf06716ea86e9135f4760b53d2653bf2d0
Attila Bartha <atikenny@gmail.com>
2025-11-08 22:08:05 +0100

    add test script for webshop item processor

--- packages/webshop-item-processor/test.js
+++ packages/webshop-item-processor/test.js
@@ -1,13 +1,17 @@
-import { handler } from './dist/src/index.js';
+import { handler } from './src/index.js';
+import { deflateSync } from 'node:zlib';
 
-await handler({
-  Records: [
-    {
-      s3: {
-        object: {
-          key: 'https://sheepandraven.com/en/our-shop'
-        }
-      }
-    }
-  ]
-});
+const url = 'https://banibeans.si/products/ethiopia-shakiso-natural';
+const roasterId = 285;
+
+const response = await fetch(url);
+
+if (!response.ok) {
+  throw new Error(`Failed to fetch webshop page ${url}`);
+}
+
+const html = (await response.text()).match(/<body[^>]*>[\s\S]*<\/body>/giu)[0];
+
+console.info(`Invoke webshop processor for ${roasterId} and url: ${url}`);
+
+await handler({ url, roasterId, html: deflateSync(html).toString('base64') });
