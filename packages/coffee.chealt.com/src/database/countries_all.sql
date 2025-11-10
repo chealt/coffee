1f74c925bad608714f8162e512ccd2f3e9034750
Attila Bartha <atikenny@gmail.com>
2025-11-10 15:17:38 +0100

    add country code

--- packages/coffee.chealt.com/src/database/countries_all.sql
+++ packages/coffee.chealt.com/src/database/countries_all.sql
@@ -1,9 +1,10 @@
 CREATE VIEW countries_all AS
 SELECT
   c.id AS country_id,
+  c.code AS code,
   l.code AS language_code,
-  ci."name"
+  ci.name
 FROM
-  "countries_i18n" ci
-  JOIN "countries" c ON c.id = ci.country_id
+  countries_i18n ci
+  JOIN countries c ON c.id = ci.country_id
   JOIN languages l ON l.id = ci.language_id;
