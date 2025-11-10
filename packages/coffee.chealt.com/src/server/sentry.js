95b8bf9df70b09d65eb9bd91212a5a3702cc4edc
Attila Bartha <atikenny@gmail.com>
2025-11-10 14:53:42 +0100

    add environment to Sentry init config

--- packages/coffee.chealt.com/src/server/sentry.js
+++ packages/coffee.chealt.com/src/server/sentry.js
@@ -10,7 +10,8 @@ const init = () => {
       dsn: 'https://c03ded959e579ac5f500b11c23ad7b1c@o4510340291821568.ingest.de.sentry.io/4510340393140304',
       // Adds request headers and IP for users, for more info visit:
       // https://docs.sentry.io/platforms/javascript/guides/astro/configuration/options/#sendDefaultPii
-      sendDefaultPii: true
+      sendDefaultPii: true,
+      environment: import.meta.env.MODE
     });
 
     console.info('Sentry initialized');
