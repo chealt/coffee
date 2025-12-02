74608630f270c4e5754cc42f8b5c3e9a56e8bcb7
Attila Bartha <atikenny@gmail.com>
2025-12-02 19:52:14 +0100

    add embrace-io for performance metrics

--- packages/coffee.chealt.com/sentry.client.config.js
+++ packages/coffee.chealt.com/sentry.client.config.js
@@ -2,6 +2,5 @@ import { init } from '@sentry/astro';
 
 init({
   dsn: 'https://c03ded959e579ac5f500b11c23ad7b1c@o4510340291821568.ingest.de.sentry.io/4510340393140304',
-  sendDefaultPii: true,
-  tracesSampleRate: 1.0
+  sendDefaultPii: true
 });
---
+++ packages/coffee.chealt.com/src/components/common/performance.js
@@ -0,0 +1,5 @@
+import { initSDK } from '@embrace-io/web-sdk';
+
+initSDK({
+  appID: 'zmmnz'
+});
