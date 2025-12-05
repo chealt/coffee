a1ba985712a531e5b50273585495d43db87dc9e8
Attila Bartha <atikenny@gmail.com>
2025-12-05 14:30:35 +0100

    update export of handler

--- packages/image-proxy/src/index.js
+++ packages/image-proxy/src/index.js
@@ -1,23 +1,25 @@
-const handler = async (request, env, ctx) => {
-  const url = new URL(request.url);
+const handler = {
+  async fetch(request, env, ctx) {
+    const url = new URL(request.url);
 
-  const cache = caches.default;
-  let response = await cache.match(request);
+    const cache = caches.default;
+    let response = await cache.match(request);
 
-  if (!response) {
-    const cloudfrontUrl = new URL(url.pathname, `https://${env.CLOUDFRONT_DOMAIN}`);
+    if (!response) {
+      const cloudfrontUrl = new URL(url.pathname, `https://${env.CLOUDFRONT_DOMAIN}`);
 
-    response = await fetch(new Request(cloudfrontUrl, request));
+      response = await fetch(new Request(cloudfrontUrl, request));
 
-    if (response.status === 200) {
-      response = new Response(response.body, response);
-      response.headers.set('Cache-Control', 'public, max-age=2592000');
+      if (response.status === 200) {
+        response = new Response(response.body, response);
+        response.headers.set('Cache-Control', 'public, max-age=2592000');
 
-      ctx.waitUntil(cache.put(request, response.clone()));
+        ctx.waitUntil(cache.put(request, response.clone()));
+      }
     }
-  }
 
-  return response;
+    return response;
+  }
 };
 
 export default handler;
