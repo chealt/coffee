593901d6c554a1ec417a27e9952e0f44ac99b661
Attila Bartha <atikenny@gmail.com>
2025-12-16 10:17:17 +0100

    remove server side tracing for Sentry, log env in Sentry init

--- packages/coffee.chealt.com/sentry.server.config.js
+++ packages/coffee.chealt.com/sentry.server.config.js
@@ -1,8 +1,9 @@
 import { init } from '@sentry/astro';
 
+// eslint-disable-next-line no-console
+console.info(`Initializing Sentry for env: ${import.meta.env.MODE}`);
 init({
   dsn: 'https://c03ded959e579ac5f500b11c23ad7b1c@o4510340291821568.ingest.de.sentry.io/4510340393140304',
   sendDefaultPii: true,
-  tracesSampleRate: 1.0,
   environment: import.meta.env.MODE
 });
