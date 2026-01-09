a4142dc490f62bfcb1bb4495cbac25971a8f831f
Attila Bartha <atikenny@gmail.com>
2026-01-09 13:41:34 +0100

    include username in error when no database url is found

--- packages/coffee.chealt.com/src/server/database/client.js
+++ packages/coffee.chealt.com/src/server/database/client.js
@@ -21,6 +21,10 @@ const getClient = (username) => {
     process.env[`TURSO_AUTH_TOKEN_${username.toUpperCase()}`] ||
     import.meta.env[`TURSO_AUTH_TOKEN_${username.toUpperCase()}`];
 
+  if (!url) {
+    throw new Error(`No database URL found for user ${username}`);
+  }
+
   try {
     const client = createClient({ url, authToken });
 
