839ed544e3f2d4971fc1f3a1c9c684daf350f3fe
Attila Bartha <atikenny@gmail.com>
2026-01-12 16:06:31 +0100

    create common component for filtered coffee pages, encode brewing method in URL

--- packages/coffee.chealt.com/src/components/utils/url.js
+++ packages/coffee.chealt.com/src/components/utils/url.js
@@ -2,10 +2,21 @@ import { getRelativeLocaleUrl } from 'astro:i18n';
 
 import { encodeSafeURL } from '../../utils/string.js';
 
-const getSafeUrl = ({ locale, brewingMethod, country, originCountry, processingMethod, variety }) =>
+const getSafeUrl = ({ locale, brewingMethod, brewingMethodGroup, country, originCountry, processingMethod, variety }) =>
   getRelativeLocaleUrl(
     locale,
-    `${brewingMethod ? `/brewing-methods/${brewingMethod}` : ''}${country ? `/countries/${encodeSafeURL(country)}` : ''}${originCountry ? `/origin-countries/${encodeSafeURL(originCountry)}` : ''}${processingMethod ? `/processing-methods/${processingMethod}` : ''}${variety ? `/varieties/${variety}` : ''}`
+    `${brewingMethod ? `/brewing-methods/${encodeSafeURL(brewingMethod)}` : ''}${brewingMethodGroup ? `/brewing-method-groups/${encodeSafeURL(brewingMethodGroup)}` : ''}${country ? `/countries/${encodeSafeURL(country)}` : ''}${originCountry ? `/origin-countries/${encodeSafeURL(originCountry)}` : ''}${processingMethod ? `/processing-methods/${processingMethod}` : ''}${variety ? `/varieties/${variety}` : ''}`
   );
 
-export { getSafeUrl };
+const getSafeUrlWithStats = ({
+  locale,
+  brewingMethod,
+  brewingMethodGroup,
+  country,
+  originCountry,
+  processingMethod,
+  variety
+}) =>
+  `${getSafeUrl({ locale, brewingMethod, brewingMethodGroup, country, originCountry, processingMethod, variety })}stats`;
+
+export { getSafeUrl, getSafeUrlWithStats };
