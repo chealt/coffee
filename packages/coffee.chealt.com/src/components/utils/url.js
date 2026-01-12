02d00674e55c90ea3b46cd5662696888b75f1a0e
Attila Bartha <atikenny@gmail.com>
2026-01-12 19:34:19 +0100

    add roaster to filtered list component

--- packages/coffee.chealt.com/src/components/utils/url.js
+++ packages/coffee.chealt.com/src/components/utils/url.js
@@ -2,10 +2,19 @@ import { getRelativeLocaleUrl } from 'astro:i18n';
 
 import { encodeSafeURL } from '../../utils/string.js';
 
-const getSafeUrl = ({ locale, brewingMethod, brewingMethodGroup, country, originCountry, processingMethod, variety }) =>
+const getSafeUrl = ({
+  locale,
+  brewingMethod,
+  brewingMethodGroup,
+  country,
+  originCountry,
+  processingMethod,
+  roaster,
+  variety
+}) =>
   getRelativeLocaleUrl(
     locale,
-    `${brewingMethod ? `/brewing-methods/${encodeSafeURL(brewingMethod)}` : ''}${brewingMethodGroup ? `/brewing-method-groups/${encodeSafeURL(brewingMethodGroup)}` : ''}${country ? `/countries/${encodeSafeURL(country)}` : ''}${originCountry ? `/origin-countries/${encodeSafeURL(originCountry)}` : ''}${processingMethod ? `/processing-methods/${processingMethod}` : ''}${variety ? `/varieties/${variety}` : ''}`
+    `${brewingMethod ? `/brewing-methods/${encodeSafeURL(brewingMethod)}` : ''}${brewingMethodGroup ? `/brewing-method-groups/${encodeSafeURL(brewingMethodGroup)}` : ''}${country ? `/countries/${encodeSafeURL(country)}` : ''}${originCountry ? `/origin-countries/${encodeSafeURL(originCountry)}` : ''}${processingMethod ? `/processing-methods/${processingMethod}` : ''}${roaster ? `/roasters/${encodeSafeURL(roaster)}` : ''}${variety ? `/varieties/${variety}` : ''}`
   );
 
 const getSafeUrlWithStats = ({
@@ -15,8 +24,9 @@ const getSafeUrlWithStats = ({
   country,
   originCountry,
   processingMethod,
+  roaster,
   variety
 }) =>
-  `${getSafeUrl({ locale, brewingMethod, brewingMethodGroup, country, originCountry, processingMethod, variety })}stats`;
+  `${getSafeUrl({ locale, brewingMethod, brewingMethodGroup, country, originCountry, processingMethod, roaster, variety })}stats`;
 
 export { getSafeUrl, getSafeUrlWithStats };
