655f9f0a2964104fb3319ce6048701d2dff318b5
Attila Bartha <atikenny@gmail.com>
2026-01-22 06:45:43 +0100

    remove encoding and decoding from varieties

--- packages/coffee.chealt.com/src/components/utils/url.js
+++ packages/coffee.chealt.com/src/components/utils/url.js
@@ -16,7 +16,7 @@ const getSafeUrl = ({
 }) =>
   getRelativeLocaleUrl(
     locale,
-    `${brewingMethod ? `/brewing-methods/${encodeSafeURL(brewingMethod)}` : ''}${brewingMethodGroup ? `/brewing-method-groups/${encodeSafeURL(brewingMethodGroup)}` : ''}${country ? `/countries/${encodeSafeURL(country)}` : ''}${originCountry ? `/origin-countries/${encodeSafeURL(originCountry)}` : ''}${processingMethod ? `/processing-methods/${encodeSafeURL(processingMethod)}` : ''}${roaster ? `/roasters/${encodeSafeURL(roaster)}` : ''}${tasteNote ? `/taste-notes/${encodeSafeURL(tasteNote)}` : ''}${tasteNoteGroup ? `/taste-note-groups/${encodeSafeURL(tasteNoteGroup)}` : ''}${variety ? `/varieties/${encodeSafeURL(variety.toLowerCase())}` : ''}`
+    `${brewingMethod ? `/brewing-methods/${encodeSafeURL(brewingMethod)}` : ''}${brewingMethodGroup ? `/brewing-method-groups/${encodeSafeURL(brewingMethodGroup)}` : ''}${country ? `/countries/${encodeSafeURL(country)}` : ''}${originCountry ? `/origin-countries/${encodeSafeURL(originCountry)}` : ''}${processingMethod ? `/processing-methods/${encodeSafeURL(processingMethod)}` : ''}${roaster ? `/roasters/${encodeSafeURL(roaster)}` : ''}${tasteNote ? `/taste-notes/${encodeSafeURL(tasteNote)}` : ''}${tasteNoteGroup ? `/taste-note-groups/${encodeSafeURL(tasteNoteGroup)}` : ''}${variety ? `/varieties/${variety.toLowerCase()}` : ''}`
   );
 
 const getSafeUrlWithStats = ({
