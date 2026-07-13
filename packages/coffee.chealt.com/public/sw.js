c5d77fc48033554ad8f504b2edffde4895f25412
Attila Bartha <atikenny@gmail.com>
2026-07-13 19:53:42 +0200

    use switch for service worker methods

--- packages/coffee.chealt.com/public/sw.js
+++ packages/coffee.chealt.com/public/sw.js
@@ -58,73 +58,94 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.action === 'cache-image') {
-    const { filename, fileData, getSignedUrl, imageUploadUrls, buffer } = event.data;
+const uploadAndCacheImage = (event) => {
+  const { filename, fileData, getSignedUrl, imageUploadUrls, buffer } = event.data;
 
-    console.info(`SW: Received image "${filename}"`);
+  console.info(`SW: Received image "${filename}"`);
 
-    event.waitUntil(
-      caches
-        .open(CACHE_NAME)
-        .then((cache) => {
-          console.info(`SW: Caching image with MIME type "${fileData.type}"`);
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        console.info(`SW: Caching image with MIME type "${fileData.type}"`);
 
-          return Promise.all(
-            imageUploadUrls.map((imageUrl) => {
-              // must create a new Response for all cache items
-              const responseToCache = new Response(buffer, {
-                headers: {
-                  'Access-Control-Allow-Origin': '*',
-                  'Content-Type': fileData.type,
-                  'Content-Length': buffer.byteLength
-                }
-              });
+        return Promise.all(
+          imageUploadUrls.map((imageUrl) => {
+            // must create a new Response for all cache items
+            const responseToCache = new Response(buffer, {
+              headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': fileData.type,
+                'Content-Length': buffer.byteLength
+              }
+            });
 
-              return cache.put(`${imageUrl.slice(0, -5)}${filename}${imageUrl.slice(-5)}`, responseToCache);
-            })
-          );
-        })
-        .then(() => console.info('✅ SW: Image cached successfully!'))
-        .then(() => {
-          console.info('SW: Uploading image...');
-
-          return uploadFile({ filename, fileData, getSignedUrl });
-        })
-        .then((response) => {
-          if (response.ok) {
-            console.info('✅ SW: Image uploaded successfully!');
-
-            return response;
-          }
-
-          console.error('SW: Image upload failed:', response);
-
-          // retry once
-          return uploadFile({ filename, fileData, getSignedUrl });
-        })
-        .catch((err) => console.error('SW: Caching failed:', err))
-    );
-  } else if (event.data && event.data.action === 'cache-collection-item') {
-    const { collectionId, itemId, filename } = event.data;
-
-    console.info(`SW: Received collection item ${filename}.`);
-
-    event.waitUntil(
-      setItem(collectionId ? addImageToCollection : addImageToCollectionItem, {
-        id: collectionId,
-        itemId,
-        filename
+            return cache.put(`${imageUrl.slice(0, -5)}${filename}${imageUrl.slice(-5)}`, responseToCache);
+          })
+        );
       })
-        .then(() => {
-          console.info(
-            collectionId
-              ? `SW: Added item to collection: "${collectionId}", item id "${itemId}" and filename "${filename}".`
-              : `SW: Added collection item with id "${itemId}" and filename "${filename}".`
-          );
-        })
-        .catch((err) => console.error('SW: Adding collection item failed:', err))
-    );
+      .then(() => console.info('✅ SW: Image cached successfully!'))
+      .then(() => {
+        console.info('SW: Uploading image...');
+
+        return uploadFile({ filename, fileData, getSignedUrl });
+      })
+      .then((response) => {
+        if (response.ok) {
+          console.info('✅ SW: Image uploaded successfully!');
+
+          return response;
+        }
+
+        console.error('SW: Image upload failed:', response);
+
+        // retry once
+        return uploadFile({ filename, fileData, getSignedUrl });
+      })
+      .catch((err) => console.error('SW: Caching failed:', err))
+  );
+};
+
+const addItemToCollection = (event) => {
+  const { collectionId, itemId, filename } = event.data;
+
+  console.info(`SW: Received collection item ${filename}.`);
+
+  event.waitUntil(
+    setItem(collectionId ? addImageToCollection : addImageToCollectionItem, {
+      id: collectionId,
+      itemId,
+      filename
+    })
+      .then(() => {
+        console.info(
+          collectionId
+            ? `SW: Added item to collection: "${collectionId}", item id "${itemId}" and filename "${filename}".`
+            : `SW: Added collection item with id "${itemId}" and filename "${filename}".`
+        );
+      })
+      .catch((err) => console.error('SW: Adding collection item failed:', err))
+  );
+};
+
+self.addEventListener('message', (event) => {
+  if (!event.data?.action) {
+    return undefined;
+  }
+
+  switch (event.data.action) {
+    case 'cache-image':
+      uploadAndCacheImage(event);
+
+      break;
+    case 'cache-collection-item':
+      addItemToCollection(event);
+
+      break;
+    default:
+      console.error(`Invalid action: ${event.data.action}`);
+
+      break;
   }
 });
 
