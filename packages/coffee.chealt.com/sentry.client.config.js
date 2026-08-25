e06608170873c61c5eb96a315c27dbce287d5838
Attila Bartha <atikenny@gmail.com>
2026-08-25 14:25:06 +0200

    ignore view transition errors

--- packages/coffee.chealt.com/sentry.client.config.js
+++ packages/coffee.chealt.com/sentry.client.config.js
@@ -4,5 +4,7 @@ init({
   dsn: 'https://c03ded959e579ac5f500b11c23ad7b1c@o4510340291821568.ingest.de.sentry.io/4510340393140304',
   sendDefaultPii: true,
   tracesSampleRate: 1.0,
-  environment: import.meta.env.MODE
+  environment: import.meta.env.MODE,
+  // ignore view transition errors
+  ignoreErrors: ['Transition was skipped', 'AbortError: Transition was skipped']
 });
